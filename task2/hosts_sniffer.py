b951ac1eeffe373c2b1eda692401111b25956288
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-07 00:21:26 +0800

    Update hosts_sniffer.py

--- task2/hosts_sniffer.py
+++ task2/hosts_sniffer.py
@@ -1,1 +1,12 @@
 from scapy.all import *
+
+src_spoofed = "192.168.1.144"
+dest_ip = "192.168.1.1"
+interface = "eth0"
+message = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  
+# ICMP Ping to find active hosts
+scapy.send(IP(src=src_spoofed,dst=dest_ip)/ICMP()/message)
+
+# 
+packets = scapy.sniff(iface=interface,store=true,prn=process_packet)
