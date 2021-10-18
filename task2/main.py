f38ed4cdc203830a2b6906267fb938b7f5f028f3
Muhammad Fa-aiz Bin Mohammad Fammi <2002784@sit.singaporetech.edu.sg>
2021-10-18 11:04:04 +0800

    Skeleton for main.py
    Added menu choices to choose attacks

--- task2/main.py
+++ task2/main.py
@@ -1,1 +1,28 @@
-from scapy.all import *
+import os
+
+def main():
+    choice = input('Select an attack to use: \nPress 1. ARP Spoofing Attack\nPress 2. EIGRP Attack\nPress 3. STP Attack\nPress 4. SYN Flooding Attack\nPress -> ')
+    print(f'You chose {choice} attack...')
+    if choice == '1':
+        sys = 'python arp_poisoning.py'
+        os.system(f'{sys} -h') 
+        arg = input('Enter only Command line arguments starting from -ti -> ')
+        os.system(f'{sys} {arg}')
+    elif choice == '2':
+        sys = 'python eigrp_route_poisoning.py'
+        os.system(f'{sys} -h') 
+        arg = input('Enter only Command line arguments starting from -src -> ')
+        os.system(f'{sys} {arg}')   
+    elif choice == '3':
+        sys = 'python stp_root_attack.py'
+        os.system(f'{sys} -h') 
+        arg = input('Enter only Command line arguments starting from -src -> ')
+        os.system(f'{sys} {arg}')
+    elif choice == '4':
+        sys = 'python syn_flooding.py'
+        os.system(f'{sys} -h') 
+        arg = input('Enter only Command line arguments starting from -tg -> ')
+        os.system(f'{sys} {arg}')
+    else:
+        print('Choose from options 1-4')
+
