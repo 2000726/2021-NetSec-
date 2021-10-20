c412d74d704cff9aac0a068deee798ce757152ad
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-20 14:25:59 +0800

    Update and rename hosts_sniffer.py to hosts_discovery.py

---
+++ task2/hosts_discovery.py
@@ -0,0 +1,32 @@
+from scapy.all import *
+import logging
+
+# Pre-definitions
+src_spoofed = "200.1.1.14"
+dest_network = "200.1.1."
+hosts = []
+#interface = "eth0"
+
+# Suppress Warning Output
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+# Loop Through Network to Identify Hosts.
+print("Available Hosts\n--------------------------------------")
+for i in range(1,15):
+  dest_host = dest_network + str(i)
+  
+  # ICMP Ping to find active hosts
+  message = sr1(IP(dst=dest_host)/ICMP(), timeout=1, verbose=0)
+  
+  if message:
+    print("Host: " + message.src)
+    hosts.append(message.src)
+    
+    
+# Conduct SYN Scan
+print("\nPort Scanning\n--------------------------------------")
+for x in hosts:
+    print(x)
+
+# 
+#packets = scapy.sniff(iface=interface,store=true,prn=process_packet)
