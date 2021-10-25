d808c4b7077181c283c48de18d90816a7449c942
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-25 16:22:22 +0800

    Update icmp_discovery.py

--- task2/icmp_discovery.py
+++ task2/icmp_discovery.py
@@ -2,8 +2,7 @@ from scapy.all import *
 import logging
 
 # Pre-definitions
-src_spoofed = "200.1.1.14"
-dest_network = "200.1.1."
+dest_network = "10.0.0."
 hosts = []
 
 # Suppress Warning Output
@@ -12,13 +11,14 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 # Loop Through Network to Identify Hosts.
 print("Available Hosts\n--------------------------------------")
 for i in range(1,255):
+  print("Scanning IP #" + str(i))
   dest_host = dest_network + str(i)
   
   # ICMP Ping to find active hosts
   message = sr1(IP(dst=dest_host)/ICMP(), timeout=1, verbose=0)
   
   if message:
-    print("Host: " + message.src)
+    print("\tHost: " + message.src)
     hosts.append(message.src)
     
 sys.exit(0)
